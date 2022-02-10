56cc2b5e2ccc06447b73878567ee2877c89c3e9b
Pierre G <pr.gaillot@gmail.com>
2022-02-10 20:23:40 +0100

    home computer install

--- src/app/espace.ts
+++ src/app/espace.ts
@@ -1,8 +1,4 @@
 export interface Espace {
     id: number;
     user: string;
-    color:string,
-    color1:string,
-    color2:string,
-    color3:string,
 }
--- src/app/espace/espace.component.ts
+++ src/app/espace/espace.component.ts
@@ -13,12 +13,8 @@ export class EspaceComponent implements OnInit {
   espace: Espace = {
     id: 45,
     user: "Habitant",
-    color: "#93F084",
-    color1: "#82E272",
-    color2: "#72D462",
-    color3: "#60BC51",
   };
-  
+
   constructor() { }
 
   ngOnInit() {
