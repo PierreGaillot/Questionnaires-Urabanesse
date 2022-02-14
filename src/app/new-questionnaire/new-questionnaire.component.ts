d9150f14a87c825da3140abf163f596a650a1f6b
Pierre G <pr.gaillot@gmail.com>
2022-02-14 01:03:39 +0100

    add question btn

--- src/app/new-questionnaire/new-questionnaire.component.ts
+++ src/app/new-questionnaire/new-questionnaire.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewQuestionnaireComponent implements OnInit {
 
+  isEditNewQuestion: boolean = false;
+
   constructor(
   ) { }
 
